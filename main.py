a014cb3fea7f0c53c83689dc3c73b9c7d05a6622
howardsolutions <phungthinh0946519356@gmail.com>
2025-05-19 11:20:52 +0700

    feat: add game funtionalities

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 from constants import *
 
 import pygame;
+from player import Player
 
 def main():
+    #  init the game
+    pygame.init()
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         # check if the user has closed the window and exit the game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         # fill the screen w black color
         screen.fill("black")
+        
+        # re-render the player on the screen each frame
+        player.draw(screen)
+    
         # update aka refresh the screen
         pygame.display.flip()
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60)  / 1000
 
 if __name__ == "__main__":
-    pygame.init()
     main()
